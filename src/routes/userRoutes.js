b5f5e687e1f1b2a4778a97c58dbbdfaa47c22d7a
Harsha6463 <157800250+Harsha6463@users.noreply.github.com>
2025-01-06 18:40:50 +0530

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 
 import express from 'express'
-import { createUser, deleteUser, getUserById, getUsers, updateUser } from '../controllers/userControllers.js';
+import { createUser, deleteUser, getUserById, getUsers, loginUser, updateUser } from '../controllers/userControllers.js';
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post('/signup',createUser)
 router.get('/allusers',getUsers)
+router.post('/login',loginUser)
 router.get('/:id',getUserById)
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
